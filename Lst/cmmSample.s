7b42f3684d2f5600ebf3e98511004d87eb5e693d
tctsigemura <sigemura@tokuyama.kosen-ac.jp>
2020-05-21 22:10:06 +0900

    リスト8.1のバグ訂正

--- Lst/cmmSample.s
+++ Lst/cmmSample.s
@@ -1,5 +1,5 @@
 _x      DW        1       // int x = 1;             DW命令はdataセグメントに出力
-_y      WS        1       // int y;                 DS命令はbssセグメントに出力
+_y      WS        1       // int y;                 WS命令はbssセグメントに出力
 _main   PUSH      FP      // void main() {          機械語命令は
         LD        FP,SP   //                          textセグメントに出力
         PUSH      G3      //   int z;
